03e31b48237046af1825e06bfcefb5730ff1acb0
BlackBond06 <eleazuosita007@yahoo.com>
2023-06-07 10:33:54 +0100

    added home page component

--- src/components/Pages/Home/Home.jsx
+++ src/components/Pages/Home/Home.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from "react";
+import SearchInput from "../../SearchBar/SearchInput"
+import VisitorList from "../VisitorList/VisitorList";
+import AddVisitorForm from "../../AddVisitorForm/AddVisitorForm";
+import Analytics from "../Analytics/Analytics";
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <>
+      <SearchInput />
+      <VisitorList />
+      <AddVisitorForm />
+      <Analytics />
+    </>
+  );
+};
 
-export default Home
+export default Home;
