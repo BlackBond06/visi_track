cd10477f364622a236a48bb8b225badbdb7213e3
BlackBond06 <eleazuosita007@yahoo.com>
2023-06-19 20:45:51 +0100

    web sockets to handle sending of events

---
+++ socket/index.js
@@ -0,0 +1,50 @@
+import { Server } from "socket.io";
+
+const io = new Server({
+  cors: {
+    origin: "http://localhost:3000",
+  },
+});
+
+let onlineUsers = [];
+
+const addNewUser = (userName, socketId) => {
+  !onlineUsers.some((user) => user.userName === userName) &&
+    onlineUsers.push({ userName, socketId });
+};
+
+
+
+const removeUser = (socketId) => {
+  onlineUsers = onlineUsers.filter((user) => user.socketId !== socketId);
+};
+
+const getUser = (userName) => {
+  return onlineUsers.find((user) => user.userName === userName);
+};
+
+io.on("connection", (socket) => {
+  socket?.on("newUser", (userName) => {
+  addNewUser(userName, socket.id);
+ 
+  });
+
+  
+
+  socket.on("sendNotification", ({ senderName, receiverName }) => {
+    const receiver = getUser(receiverName);
+    
+
+    io.emit("getNotification", {
+      senderName,
+      receiverName
+    });
+  });
+
+  socket.on("disconnect", () => {
+    removeUser(socket.id);
+   
+  });
+});
+
+io.listen(5000);
