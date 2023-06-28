f92ebbf81f31bf9abaffdeea9ced269b94574c08
BlackBond06 <eleazuosita007@yahoo.com>
2023-06-28 14:24:04 +0100

    Retrieve staff data from firestore db

---
+++ src/components/Pages/r/[staffId]/index.jsx
@@ -0,0 +1,46 @@
+import { doc, getDoc } from "firebase/firestore";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { firestore } from "../../../../firebase/clientApp";
+import { visitorAtomState } from "../../../../atoms/visitorsAtom";
+import StaffNotFound from "../../../Staff/NotFound";
+
+const StaffPage = () => {
+  const [data, setData] = useState(null);
+ 
+  const { userId } = useParams();
+
+  useEffect(() => {
+    async function getServerSideProps() {
+      try {
+        const vistorDocRef = doc(firestore, "visitorDetails", userId);
+
+        const visitorDoc = await getDoc(vistorDocRef);
+
+        return setData({
+          data: {
+            visitorData: visitorDoc.exists()
+              ? JSON.parse(
+                  JSON.stringify({ id: visitorDoc.id, ...visitorDoc.data() })
+                )
+              : "",
+          },
+        });
+      } catch (error) {
+        console.log("getServerSide error:", error);
+      }
+    }
+
+    getServerSideProps();
+  }, [userId]);
+
+  if(!data?.data.visitorData){
+    return(
+     <StaffNotFound/>
+    )
+  }
+
+  return <div>Welcome To {userId}</div>;
+};
+
+export default StaffPage;
