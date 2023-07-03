285697fc01c3100a1bce9fca88d61a27dfb0d306
BlackBond06 <eleazuosita007@yahoo.com>
2023-07-03 18:15:46 +0100

    add route animation with framer-motion

--- src/components/Pages/ClientProfile/ClientProfile.jsx
+++ src/components/Pages/ClientProfile/ClientProfile.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from "react";
+
+import { motion } from "framer-motion";
+
+const routeVariants = {
+  initial: {
+    y: "100vh",
+  },
+  final: {
+    y: "0vh",
+    transition: {
+      type: "spring",
+      mass: "0.4",
+    },
+  },
+};
 
 const ClientProfile = () => {
   return (
-    <div>ClientProfile</div>
-  )
-}
+    <motion.div variants={routeVariants} initial="initial" animate="final">
+      ClientProfile
+    </motion.div>
+  );
+};
 
-export default ClientProfile
+export default ClientProfile;
