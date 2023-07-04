664227228f315051441a2aba944c3fd4405d5a13
BlackBond06 <eleazuosita007@yahoo.com>
2023-07-04 17:12:37 +0100

    Add handleVerifyStaff function staff auth

--- src/components/Modal/Auth/StaffAuth.jsx
+++ src/components/Modal/Auth/StaffAuth.jsx
@@ -1,8 +1,37 @@
 import { Button, Flex, Icon, Input, Text } from "@chakra-ui/react";
-import React from "react";
+import React, { useState } from "react";
 import { FaUsers } from "react-icons/fa";
+import { useNavigate } from "react-router-dom";
+import { useSetRecoilState } from "recoil";
+import { authModalState } from "../../../atoms/authModalAtom";
 
 const StaffAuth = () => {
+  const setAuthModalState = useSetRecoilState(authModalState);
+  const [error, setError] = useState("");
+  const [input, setInput] = useState({
+    name:"",
+    password:""
+  });
+
+  // useNavigate hook for routing
+  let navigate = useNavigate();
+
+  const handleChange = (event)=>{
+    setInput(prev =>({
+      ...prev,
+      [event.target.name]: event.target.value
+    }))
+  }
+
+  const handleVerifyStaff = (event)=>{
+    event.preventDefault();
+    if(input.name != "Friday" && input.password != "1234"){
+      setError("Access Denied! Invalid username or password")
+      return;
+    }
+    setAuthModalState({open:false});
+    navigate(`/r/${input.name}`);
+  }
   return (
     <Flex direction="column" align="center" width="100%">
       <Flex direction="column" align="center">
@@ -13,14 +42,14 @@ const StaffAuth = () => {
 
       <Flex direction="column" align="center" width="inherit">
         <Icon as={FaUsers} color="gold.200" fontSize={40} mb={2} />
-        <form onSubmit={() => {}} style={{ width: "100%" }}>
+        <form onSubmit={handleVerifyStaff} style={{ width: "100%" }}>
           <Input
             required
             name="name"
             placeholder="staff name"
             type="text"
             mb={2}
-            // onChange={(event) => setEmail(event.target.value)}
+            onChange={handleChange}
             fontSize="10pt"
             _placeholder={{ color: "gray.500" }}
             _hover={{
@@ -42,7 +71,7 @@ const StaffAuth = () => {
             name="password"
             placeholder="password"
             type="password"
-            // onChange={onChange}
+            onChange={handleChange}
             fontSize="10pt"
             _placeholder={{ color: "brand.500" }}
             _hover={{
@@ -58,9 +87,9 @@ const StaffAuth = () => {
             }}
             bg="gray.50"
           />
-          {/* <Text textAlign="center" fontSize="10pt" color="red">
-            {error?.message}
-          </Text> */}
+          {<Text textAlign="center" fontSize="10pt" color="red">
+            {error}
+          </Text>}
           <Button
             width="100%"
             height="36px"
