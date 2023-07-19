b4ba987f3c860d20d521f2b613db682f2036dd30
BlackBond06 <eleazuosita007@yahoo.com>
2023-07-19 13:33:44 +0100

    displays dashboard to only clients

--- src/components/RightContent/UserMenu.jsx
+++ src/components/RightContent/UserMenu.jsx
@@ -39,6 +39,8 @@ const UserMenu = ({ user, notifications, open, setOpen, socket }) => {
 
   }
 
+  // displays dasboard page to only clients
+  const showProfile = user?.displayName || user?.email.split("@")[0];
    // useNavigate hook for routing
    let navigate = useNavigate();
 
@@ -79,6 +81,7 @@ const UserMenu = ({ user, notifications, open, setOpen, socket }) => {
               fontSize="10pt"
               fontWeight={700}
               _hover={{ bg: "electric.200", color: "white" }}
+              display={(showProfile === "chike" || showProfile === "nugo") ? "none" : "block"}
             >
               <Flex align="center" onClick={()=> navigate("/profile")}>
                 {user?.photoURL ? <Image width="20px" mr={2} src={user.photoURL} borderRadius="50%"/> : <Icon fontSize="20px" mr={2} as={CgProfile} />}
