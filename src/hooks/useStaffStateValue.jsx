1b806073184dcef68f5126614d9c335156aae6be
BlackBond06 <eleazuosita007@yahoo.com>
2023-07-23 09:04:39 +0100

    Show staff page button or client  check in button

--- src/hooks/useStaffStateValue.jsx
+++ src/hooks/useStaffStateValue.jsx
@@ -3,7 +3,7 @@ import { useRecoilState } from "recoil";
 import { visitorState } from "../atoms/visitorsAtom";
 import { auth, firestore } from "../firebase/clientApp";
 
-import { collection, getDocs } from "firebase/firestore";
+import { collection, doc, getDocs, writeBatch } from "firebase/firestore";
 import { useAuthState } from "react-firebase-hooks/auth";
 
 export const useStaffStateValue = () => {
@@ -14,13 +14,13 @@ export const useStaffStateValue = () => {
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState("");
 
-
+ 
  
   useEffect(()=>{
     if(!user) return;
 
     const getMySnippets = async () => {
-      setLoading(false);
+      setLoading(true);
       try {
         const snippetDocs = await getDocs(
           collection(firestore, `users/${user?.uid}/visitorSnippets`)
@@ -42,8 +42,32 @@ export const useStaffStateValue = () => {
   }, [setStaffStateValue, user]);
 
 
+  const checkOut = async (id)=> {
+    setLoading(true);
+    try {
+      const batch = writeBatch(firestore);
+    batch.delete(doc(firestore, `users/${user?.uid}/visitorSnippets`, id));
+      
+     
+      await batch.commit();
+      
+      console.log(id);
+      setStaffStateValue(prev => ({
+        ...prev,
+        mySnippets: prev.mySnippets.filter(item => item.visitorId !== id)
+      }))
+    } catch (error) {
+      console.log("checkOut Error:", error.message);
+      setError(error.message);
+    }
+
+    setLoading(false);
+  }
+
+
   return {
     staffStateValue,
     loading,
+    checkOut
   };
 };
